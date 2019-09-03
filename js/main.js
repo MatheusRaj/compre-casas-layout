6291cadc75f1e2ca5cd3573c5a575afd6d3fc82e
matheusrajeduzz <matheus.mendes@eduzz.com>
2019-09-02 21:05:12 -0300

    fixed prices format

--- js/main.js
+++ js/main.js
@@ -111,14 +111,14 @@ jQuery(document).ready(function($) {
     $( "#slider-range" ).slider({
       range: true,
       min: 0,
-      max: 100000,
-      values: [ 0, 15000 ],
+      max: 1000000,
+      values: [ 0, 150000 ],
       slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        $( "#amount" ).val( "R$" + ui.values[ 0 ] + " - R$" + ui.values[ 1 ] );
       }
     });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+    $( "#amount" ).val( "R$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - R$" + $( "#slider-range" ).slider( "values", 1 ) );
 	};
 	siteSliderRange();
 
