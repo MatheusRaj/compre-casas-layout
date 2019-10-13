813cb7c89871a21c185f02ba46c4237dc38351c9
matheusrajeduzz <matheus.mendes@eduzz.com>
2019-10-13 12:58:33 -0300

    created email send script

--- contact_form.php
+++ contact_form.php
@@ -1,23 +1,21 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$message = $_POST['message'];
+   // Check for empty fields
+   if(empty($_POST['name'])  		||
+      empty($_POST['email']) 		||
+      empty($_POST['message'])	||
+      !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
+         return false;
+      }
+      
+   $name = $_POST['name'];
+   $email_address = $_POST['email'];
+   $message = $_POST['message'];
 
-$to = 'valdomiroaparecidolopes@gmail.com';
-$email_subject = "Novo cliente:  $name";
-$email_body = "Novo email recebido do site institucional.\n\n"."Detalhes do cliente:\n\nNome: $name\n\nEmail: $email_address\n\nMensagem:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+   $to = 'matheusraj5@gmail.com';
+   $email_subject = "Novo cliente:  $name";
+   $email_body = "Novo email recebido do site institucional.\n\n"."Detalhes do cliente:\n\nNome: $name\n\nEmail: $email_address\n\nMensagem:\n$message";
+   $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+   $headers .= "Reply-To: $email_address";	
+   mail($to,$email_subject,$email_body,$headers);
+   return true;			
 ?>
--- js/contact_form.js
+++ js/contact_form.js
@@ -1,3 +1,28 @@
 jQuery(function () {
-    var form = $('#ajax-contact-form')
+    var form = $('#ajax-contact-form');
+
+    form.on('submit', e => {
+        if (!e.isDefaultPrevented()) {
+            const url = 'contact.php';
+
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                sucess: (data) => {
+                    let messageAlert = 'alert-' + data.type;
+                    let messageText = data.message;
+
+                    let alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    
+                    if (messageAlert && messageText) {
+                        form.find('.messages').html(alertBox);
+
+                        form[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    })
 })
