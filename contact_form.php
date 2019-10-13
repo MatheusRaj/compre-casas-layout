af167f3f770628735aafdd21f023d2bec4bafd9b
matheusrajeduzz <matheus.mendes@eduzz.com>
2019-10-13 14:58:17 -0300

    contact message fixed

--- contact_form.php
+++ contact_form.php
@@ -1,40 +1,31 @@
 <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        # FIX: Replace this email with recipient email
-        $mail_to = "matheusraj5@gmail.com";
+    $mail_to = "matheusraj5@gmail.com";
         
-        # Sender Data
-        $subject = trim($_POST["subject"]);
-        $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
-        $email = $_POST["email"];
-        $phone = trim($_POST["phone"]);
-        $message = trim($_POST["message"]);
-        
-        # Mail Content
-        $content = "Name: $name\n";
-        $content .= "Email: $email\n\n";
-        $content .= "Phone: $phone\n";
-        $content .= "Message:\n$message\n";
+    # Sender Data
+    $subject = $_POST["subject"];
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $message = $_POST["message"];
+    
+    # Mail Content
+    $content = "Name: $name\n";
+    $content .= "Email: $email\n\n";
+    $content .= "Phone: $phone\n";
+    $content .= "Message:\n$message\n";
 
-        # email headers.
-        $headers = "From: $name &lt;$email&gt;";
+    # email headers.
+    $headers = "From: " . $name . "<". $email .">\r\n";
 
-        # Send the email.
-        $success = mail($mail_to, $subject, $content, $headers);
-        if ($success) {
-            # Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thank You! Your message has been sent.";
-        } else {
-            # Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong, we couldn't send your message.";
-        }
-
-        } else {
-            # Not a POST request, set a 403 (forbidden) response code.
-            http_response_code(403);
-            echo "There was a problem with your submission, please try again.";
-        }
+    # Send the email.
+    $success = mail($mail_to, $subject, $content, $headers);
+    if ($success) {
+        # Set a 200 (okay) response code.
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    } else {
+        # Set a 500 (internal server error) response code.
+        http_response_code(500);
+        echo "Oops! Something went wrong, we couldn't send your message.";
+    }
 ?>
