6744ecf38dcfde56fd42b3283becb55f09b24014
matheusrajeduzz <matheus.mendes@eduzz.com>
2019-10-13 14:46:51 -0300

    contact message fixed

--- contact_form.php
+++ contact_form.php
@@ -7,16 +7,9 @@
         # Sender Data
         $subject = trim($_POST["subject"]);
         $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $email = $_POST["email"];
         $phone = trim($_POST["phone"]);
         $message = trim($_POST["message"]);
-        
-        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($phone) OR empty($subject) OR empty($message)) {
-            # Set a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo "Please complete the form and try again.";
-            exit;
-        }
         
         # Mail Content
         $content = "Name: $name\n";
